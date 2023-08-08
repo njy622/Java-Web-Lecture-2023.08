ef3f1079381909bf8585d0b94c19e8d43683a24f
njy622 <njy622@naver.com>
2023-08-08 17:30:57 +0900

    2023.08.08(JavaWeb)

---
+++ demo/src/main/java/com/example/demo/DemoController.java
@@ -0,0 +1,41 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import jakarta.servlet.http.HttpServletRequest;
+
+@Controller
+public class DemoController {
+	
+	// localhost:8080/demo/index	
+	@GetMapping("/index")
+	@ResponseBody
+	public String index() {
+		return "<h1>Hello World!!!</h1>";
+	}
+
+	@GetMapping("/hello")
+	public String hello() {
+		return "01.hello";		
+	}
+	
+	@GetMapping("/login")  //FROM 화면 보여주기   //입력한 값을 처리해주는게 필요
+	public String login() {
+		return "02.login";
+	}
+	@PostMapping("/login")
+//	@ResponseBody
+	public String loginProc(HttpServletRequest req, Model model) {
+		String uid = req.getParameter("uid"); //"uid"가 input tag의 name 값
+		String pwd = req.getParameter("pwd");
+//		return "<h1>uid="+ uid+", pwd="+pwd+"</h1>";
+		
+		model.addAttribute("uid",uid);
+		model.addAttribute("pwd",pwd);
+		
+		return "03.loginResult";
+	}
+}
---
+++ demo/src/main/webapp/WEB-INF/view/01.hello.jsp
@@ -0,0 +1,16 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html lang="en">
+<head>              
+    <!-- 시작은 !+tab -->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello</title>
+</head>
+<body>
+    <h1>안녕하세요?</h1>
+    <hr>
+    <h3>HTML 첫 파일입니다.</h3><br>
+    <h3>Hello, World!!!</h3>
+</body>
+</html>
