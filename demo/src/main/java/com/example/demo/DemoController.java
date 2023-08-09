e32b42d06504b6cec5aa793b33e38c623470492b
njy622 <njy622@naver.com>
2023-08-09 10:51:26 +0900

    2023.08.09(스프링,HTML)

--- demo/src/main/java/com/example/demo/DemoController.java
+++ demo/src/main/java/com/example/demo/DemoController.java
@@ -38,4 +38,51 @@ public class DemoController {
 		
 		return "03.loginResult";
 	}
+	@GetMapping("/getParam")
+	@ResponseBody
+	// GET 방식을 이용해서 주소에 정보를 입력하여 화면에 값을 나타내는 방식
+	public String getParam(HttpServletRequest req) {
+		//파라미터를 하려면 @ResponseBody와 HttpServletRequest req를 붙여줌
+		String a_ = req.getParameter("a"); //임시변수 표시는 언더바표시해줌(변수명_)
+		String b_ = req.getParameter("b");
+		String op = req.getParameter("op");
+		int a = Integer.parseInt(a_);   
+		//문자열을 숫자로 타입변환(Integer.parseInt를 사용) (int)는 숫자는 가능하지만 문자열은 안됨
+		int b = Integer.parseInt(b_);
+		int result=0;
+		String oper = "";
+		
+		switch(op) {
+		case "add" : result = a + b; oper= "+"; 			break;
+		case "sub" : result = a - b; oper= "-";				break;
+		case "mul" : result = a * b; oper= "*";				break;
+		case "div" : result = (int) (a / b); oper= "/"; 	break;
+		default : result = 0;
+		}
+				
+		return "<h1>"+ a +" "+ oper +" " + b +" " + "=" + result + "</h1>";
+	}
+	//calc 폼이 화면에 보이도록함
+	@GetMapping("/calc")
+	public String calcForm(int a, int b, String op) {  //파라미터 자리에 이렇게 입력해주면 String값을 int로 바꿔줌(스프링에서만 가능한 기능)
+		return "04.calcForm";
+	}
+	
+	@PostMapping("/calc")
+	public String clacProc(int a, int b, String op, Model model) {
+		int result=0;
+		String oper = "";
+		switch(op) {
+		case "add" : result = a + b; oper= "+"; 			break;
+		case "sub" : result = a - b; oper= "-";				break;
+		case "mul" : result = a * b; oper= "*";				break;
+		case "div" : result = (int) (a / b); oper= "/"; 	break;
+		default : result = 0;
+		}
+		model.addAttribute("a", a);
+		model.addAttribute("b", b);
+		model.addAttribute("oper", oper);
+		model.addAttribute("result", result);
+		return "05.calcResult";
+	}
 }
