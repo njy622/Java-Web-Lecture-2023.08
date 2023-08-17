a0c77bc363c02d093588eefe4b29a52acb44dff1
njy622 <njy622@naver.com>
2023-08-17 14:43:08 +0900

    2023.08.17

--- demo/src/main/java/com/example/demo/JspController.java
+++ demo/src/main/java/com/example/demo/JspController.java
@@ -1,16 +1,21 @@
 package com.example.demo;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
+import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import jakarta.servlet.http.HttpSession;
 
 @Controller  // 콘트롤러 사용하려면 가장 먼저 추가해야함 
 @RequestMapping("/jsp")  //주소로 불러오고 싶을때 => /demo/jsp
@@ -81,6 +86,46 @@ public class JspController {
 		list.add(m1); list.add(m2);
 		model.addAttribute("memberList", list);
 		return "jsp/el4_pojo";
+	}
+	
+	@GetMapping("/jstlCore")
+	public String jstlCore(Model model) {
+		Address addr1 = new Address(12345, "LA", "미국");
+		Address addr2 = new Address(67890, "뉴욕", "미국");
+		Member m1 = new Member(101, "제임스", addr1);
+		Member m2 = new Member(102, "마리아", addr2);
+		Member m3 = new Member(103, "홍길동", new Address(23456, "서울", "한국"));
+		Member m4 = new Member(104, "김자바", new Address(23456, "고양", "한국"));
+		//jsp를 보내려면.. model.
+		model.addAttribute("m1", m1);
+		model.addAttribute("m2", m2);
+		
+		Member[] member = {m1, m2};
+		model.addAttribute("memberArray",member);
+		
+		List<Member> list = new ArrayList<>();
+		list.add(m1); list.add(m2); list.add(m3); list.add(m4);
+		model.addAttribute("memberList", list);
+		return "jsp/jstl1_core";
+	}
+	
+	@GetMapping("/jstlFmt")
+	public String jstlFmt(Model model) {
+		int price = 12345000;
+		Date now = new Date();
+		model.addAttribute("price", price);
+		model.addAttribute("now", now);
+		
+		LocalDate ldNow = LocalDate.now();
+		LocalTime ltNow = LocalTime.now();
+		LocalDateTime ldtNow = LocalDateTime.now();
+		model.addAttribute("ldNow", ldNow);
+		model.addAttribute("ltNow", ltNow);
+		model.addAttribute("ltNow2", ltNow.toString().substring(0,8));
+		model.addAttribute("ldtNow", ldtNow);
+		model.addAttribute("ldtNow2", ldtNow.toString().substring(0, 19).replace("T", " "));
+		
+		return "jsp/jstl2_fmt";
 	}
 	
 }
